e362cf9cf44f917938872165a7fe033e91c09aa0
Juliana Aquino <juliana.aquino@santander.com.br>
2021-11-04 21:51:31 -0300

    adding agregador de logs

--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +14,21 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient fornecedorClient;
 
 	public Compra realizarCompra(CompraDTO compra) {
 		
-		InfoFornecedorDTO info =
-			fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		final String estado = compra.getEndereco().getEstado();
 		
+		LOG.info("buscando informação do fornecedor de {}", estado);
+		
+		InfoFornecedorDTO info =
+			fornecedorClient.getInfoPorEstado(estado);
+		
+		LOG.info("realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 		
 		System.out.println(info.getEndereco());
@@ -28,6 +37,8 @@ public class CompraService {
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
 		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		
+		System.out.println(info.getEndereco());
 		
 		return compraSalva;
 	}
